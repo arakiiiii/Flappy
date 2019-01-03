ddb1a84698867d224e7bdb3d6df8b39184f01f8b
Akira Kamite <akirakamite@akiranomacbook-puro.local>
2019-01-04 00:40:18 +0900

    Initial Commit

---
+++ Flappy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flappy
+//
+//  Created by Akira Kamite on 2019/01/04.
+//  Copyright © 2019 araki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
